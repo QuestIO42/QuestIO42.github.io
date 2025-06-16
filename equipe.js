06f72d945c1d289ff96651d1c17c900302d5a689
Enzo Murayama <enzomurayama@gmail.com>
2025-06-16 05:12:20 -0300

    refactor: team page

---
+++ equipe.js
@@ -0,0 +1,181 @@
+const equipeAtual = [
+  {
+    nome: "André Luis Zitelli Rettondini",
+    cargo: "Front-end",
+    img: "https://github.com/alrettondini.png",
+    github: "https://github.com/alrettondini",
+    linkedin: "https://www.linkedin.com/in/alrettondini/",
+    descricao: "Estudante de Ciência da Computação na UFSCar. Tenho experiência como desenvolvedor Fullstack, com foco em Front-End. Agora aprendendo Desenvolvimento Móvel com Flutter."
+  },
+  {
+    nome: "Enzo Youji Murayama",
+    cargo: "Front-end",
+    img: "https://github.com/enzomurayama.png",
+    github: "https://github.com/enzomurayama",
+    linkedin: "https://www.linkedin.com/in/enzo-youji-murayama/",
+    descricao: "Estudante de Ciência da Computação na UFSCar, agora aprendendo sobre docker e segurança. Tenho experiência com desenvolvimento fullstack."
+  },
+  {
+    nome: "João Victor Bueno de Caldas",
+    cargo: "Back-end",
+    img: "https://github.com/jobucaldas.png",
+    github: "https://github.com/jobucaldas",
+    linkedin: "https://www.linkedin.com/in/joaobuenoc/",
+    descricao: "Estudante de Ciência da Computação na UFSCar, trabalhei em desenvolvimento fullstack anteriormente."
+  },
+  {
+    nome: "Lucas de Oliveira Rodrigues Alves",
+    cargo: "Back-end",
+    img: "https://github.com/Lusquiinha.png",
+    github: "https://github.com/Lusquiinha",
+    linkedin: "https://www.linkedin.com/in/lucas-oliveira-rodrigues-alves-826600272/",
+    descricao: "Estudante de Ciência da Computação na UFSCar. Tenho experiência como desenvolvedor Fullstack, com foco em Back-End."
+  },
+  {
+    nome: "Marcus Vinicius Soares de Oliveira",
+    cargo: "Back-end",
+    img: "https://github.com/HotdogcNutela.png",
+    github: "https://github.com/HotdogcNutela",
+    linkedin: "",
+    descricao: ""
+  }
+];
+
+const membrosAnteriores = [
+  {
+    nome: "Diogo Conforti Vaz Bellini",
+    cargo: "Front-end, Design",
+    img: "https://github.com/Diogo-Bellini.png",
+    github: "https://github.com/Diogo-Bellini",
+    linkedin: "https://www.linkedin.com/in/diogo-conforti-vaz-bellini-a993b326b/",
+    descricao: "Sou estudante de Ciência da computação na UFSCar, com conhecimentos em C e C++. No momento estou entrando no mundo Front-end!!"
+  },
+  {
+    nome: "Eduardo H Spinelli",
+    cargo: "Back-end, Front-end, DevOps",
+    img: "https://github.com/Edu-Spinelli.png",
+    github: "https://github.com/Edu-Spinelli",
+    linkedin: "https://www.linkedin.com/in/eduardo-spinelli-a309011a1",
+    descricao: "Sou estudante de Ciência da computação na UFSCar, estudando sobre desenvolvimento web e entusiasta na área de sec info (crypto)"
+  },
+  {
+    nome: "Enzo Hirotani",
+    cargo: "Back-end, Front-end",
+    img: "https://github.com/EnzoHirotani.png",
+    github: "https://github.com/EnzoHirotani",
+    linkedin: "https://www.linkedin.com/in/enzo-hirotani-730a74245/",
+    descricao: "Sou estudante de Ciência da computação na UFSCar 023 e estou estudando desenvolvimento web, Python e um pouco de Java."
+  },
+  {
+    nome: "Gustavo Gimenes",
+    cargo: "Back-end, DevOps",
+    img: "https://github.com/guugimeness.png",
+    github: "https://github.com/guugimeness",
+    linkedin: "https://www.linkedin.com/in/gustavogimenes/",
+    descricao: "Sou estudante da Engenharia de Computação UFSCar 023, estou estudando Python e Docker, mas também curto muito C++."
+  },
+  {
+    nome: "Gustavo Kim Alcantara",
+    cargo: "Back-End",
+    img: "https://github.com/gus-kim.png",
+    github: "https://github.com/gus-kim",
+    linkedin: "https://www.linkedin.com/in/gustavo-kim-alcantara-869132215/",
+    descricao: "Sou Gustavo Kim, estudante de ciências da computação na UFSCar. Tenho conhecimento em C, C++ e estou me aprofundando em Python."
+  },
+  {
+    nome: "João Paulo Morais Rangel",
+    cargo: "Back-end, Front-end",
+    img: "https://github.com/jpmrangel.png",
+    github: "https://github.com/jpmrangel",
+    linkedin: "https://www.linkedin.com/in/joaopaulo-rangel",
+    descricao: "Sou estudante de Ciência da Computação na UFSCar com experiência em desenvolvimento web. Tenho conhecimento em C, C++, Java e iniciante em Python."
+  },
+  {
+    nome: "José Maia de Oliveira",
+    cargo: "Back-end, DevOps",
+    img: "https://github.com/decker12w.png",
+    github: "https://github.com/decker12w",
+    linkedin: "https://www.linkedin.com/in/jos%C3%A9-maia-1b28711b4/",
+    descricao: "Tenho experiência com desenvolvimento web. No front-end, trabalho com React e Next.js. No back-end, utilizo Node.js e Spring Boot. Também possuo conhecimentos básicos em DevOps."
+  },
+  {
+    nome: "Marlon H Sanches",
+    cargo: "Tech Lead",
+    img: "https://github.com/MarlonHenq.png",
+    github: "https://github.com/MarlonHenq",
+    linkedin: "https://www.linkedin.com/in/marlonhenq/",
+    descricao: "Estudante de Ciência da Computação na UFSCar e desenvolvedor full-stack que acaba fazendo desde design até análise de dados."
+  },
+  {
+    nome: "Maykon dos Santos",
+    cargo: "Front-End, Back-End, Dev-Ops",
+    img: "https://github.com/sgmaykon.png",
+    github: "https://github.com/sgmaykon",
+    linkedin: "https://www.linkedin.com/in/maykon-dos-santos-gon%C3%A7alves-22969120a/",
+    descricao: "Sou estudante de Engenharia de Computação na UFSCar, com conhecimentos em C e C++. Atualmente estou aprendendo ReactJS, Docker e um pouco de Python."
+  },
+  {
+    nome: "Melissa Shimada",
+    cargo: "Front-end",
+    img: "https://github.com/MelShimada.png",
+    github: "https://github.com/MelShimada",
+    linkedin: "https://www.linkedin.com/in/melissa-shimada",
+    descricao: "Sou Melissa Shimada, estudante de Ciência da Computação na UFSCar e estou estudando Javascript e HTML"
+  },
+  {
+    nome: "Murilo Ramos",
+    cargo: "Front-end",
+    img: "https://github.com/MuriEdu.png",
+    github: "https://github.com/MuriEdu",
+    linkedin: "https://www.linkedin.com/in/muriedu",
+    descricao: "Sou Murilo Ramos, estudante de BCC 023, tenho experiência tanto em front-end (React e ReactNative) quanto em back-end (Spring)"
+  },
+  {
+    nome: "Nicole Ramos",
+    cargo: "Design, Front-End",
+    img: "https://github.com/Ninn-up.png",
+    github: "https://github.com/Ninn-up",
+    linkedin: "https://www.linkedin.com/in/nicole-correa-ramos",
+    descricao: "Sou estudante de Engenharia da Computação na UFSCar e tenho experiência com design, prototipação e UI/UX. Busco aprender mais sobre as diversas áreas da computação."
+  },
+  {
+    nome: "Nicolas Queiroz Bertozzo",
+    cargo: "Backend",
+    img: "https://github.com/n-qber.png",
+    github: "https://github.com/n-qber",
+    linkedin: "https://br.linkedin.com/in/nqber",
+    descricao: "Sou estudante de Engenharia da Computação na UFSCar. Busco entender os funcionamentos por trás dos computadores e sou fã de ensinar tudo aquilo que aprendo nessa busca."
+  },
+  {
+    nome: "Renan Alves Zago",
+    cargo: "Back-End, Front-End",
+    img: "https://github.com/RenanZago.png",
+    github: "https://github.com/RenanZago",
+    linkedin: "https://www.linkedin.com/in/renan-zago/",
+    descricao: "Tenho experiência em mobile com Flutter e conhecimento em front-end web. Procurando sempre aprender."
+  },
+  {
+    nome: "Ronan Pereira",
+    cargo: "DevOps",
+    img: "https://github.com/ronanpjr.png",
+    github: "https://github.com/ronanpjr",
+    linkedin: "https://www.linkedin.com/in/ronan-antonio-pereira-junior-92165b2b1",
+    descricao: "Sou estudante de Ciência da Computação na UFSCar e tenho experiência com Linux e desenvolvimento C#. Tenho interesse desde o baixo nível até coisas como design patterns."
+  },
+  {
+    nome: "Vitor Taichi Taira",
+    cargo: "Back-End, DevOps",
+    img: "https://github.com/TaiFile.png",
+    github: "https://github.com/TaiFile",
+    linkedin: "https://www.linkedin.com/in/vitor-taichi-taira-02416b259/",
+    descricao: "Faço Ciência da Computação 023 na UFSCar e tenho conhecimento em C e C++. Quero expandir para desenvolvimento web e aprender outras tecnologias."
+  },
+  {
+    nome: "Vitória Hilgert Tomasel",
+    cargo: "Back-End, Front-End",
+    img: "https://github.com/vitoriahilgert.png",
+    github: "https://github.com/vitoriahilgert",
+    linkedin: "https://www.linkedin.com/in/vit%C3%B3ria-hilgert-2a8867307/",
+    descricao: "Tenho um pouco de experiência com Python, SQL e front-end e busco expandir meus conhecimentos em desenvolvimento web."
+  }
+];
